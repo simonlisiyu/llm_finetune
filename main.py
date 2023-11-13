19f293dfa6df1944d3ac1b624a93480201eeff4d
lisiyu <lisiyu@haizhi.com>
2023-11-13 20:41:53 +0800

    配置进一步精简

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 from fastapi import FastAPI
 import uvicorn
 from trainer.api.view.index_view import router as index_router
-from trainer.api.view.data_view import router as data_router
-from trainer.api.view.finetune_view import router as finetune_router
-from trainer.api.view.merge_view import router as merge_router
-from trainer.api.view.eval_view import router as eval_router
-from trainer.api.view.chat_view import router as chat_router
-from trainer.api.view.script_view import router as script_router
-from trainer.api.api_task import router as api_router
-from trainer.api.api_model import router as api_model
+# from trainer.api.view.data_view import router as data_router
+# from trainer.api.view.finetune_view import router as finetune_router
+# from trainer.api.view.merge_view import router as merge_router
+# from trainer.api.view.eval_view import router as eval_router
+# from trainer.api.view.chat_view import router as chat_router
+# from trainer.api.view.script_view import router as script_router
+# from trainer.api.api_task import router as api_router
+# from trainer.api.api_model import router as api_model
 from trainer.settings import Settings
 
 app = FastAPI()
 app.include_router(index_router)
-app.include_router(data_router)
-app.include_router(finetune_router)
-app.include_router(merge_router)
-app.include_router(eval_router)
-app.include_router(chat_router)
-app.include_router(script_router)
-app.include_router(api_router)
-app.include_router(api_model)
+# app.include_router(data_router)
+# app.include_router(finetune_router)
+# app.include_router(merge_router)
+# app.include_router(eval_router)
+# app.include_router(chat_router)
+# app.include_router(script_router)
+# app.include_router(api_router)
+# app.include_router(api_model)
 my_settings = Settings()
 
 
--- trainer/settings.py
+++ trainer/settings.py
@@ -28,27 +28,27 @@ class Settings(metaclass=Singleton):
             self.chat_url = self.controller_url + "/chat"
 
             # worker
-            self.worker_model_path = yaml_conf["worker"]["model_dir"]
+            self.worker_model_path = yaml_conf["trainer"]["base_dir"] + "llm/"
 
             # trainer
-            self.train_ports = yaml_conf["trainer"]["ports"]
+            self.train_ports = [9901, 9902, 9903, 9904, 9905, 9906, 9907, 9908, 9909, 9910]
 
             self.base_dir = yaml_conf["trainer"]["base_dir"]
-            self.data_dir = yaml_conf["trainer"]["data_dir"]
-            self.model_dir = yaml_conf["trainer"]["model_dir"]
-            self.log_dir = yaml_conf["trainer"]["log_dir"]
-            self.dev_script_dir = yaml_conf["trainer"]["dev_script_dir"]
+            self.data_dir = 'data/'
+            self.model_dir = 'llm/'
+            self.log_dir = 'logs/'
+            self.dev_script_dir = 'scripts/dev/'
 
-            self.data_file = yaml_conf["trainer"]["data_file"]
-            self.model_file = yaml_conf["trainer"]["model_file"]
+            self.data_file = "data/dataset_info.json"
+            self.model_file = "config/model_info.json"
 
             self.base_data_path = self.base_dir + self.data_dir
             self.base_model_path = self.base_dir + self.model_dir
 
             # redis
-            self.redis_ip = yaml_conf["redis"]["ip"]
-            self.redis_port = yaml_conf["redis"]["port"]
-            self.redis_password = yaml_conf["redis"]["password"]
+            self.redis_ip = '127.0.0.1'
+            self.redis_port = 6379
+            self.redis_password = ""
 
         # 常量
         self.datatime_sft = '%Y-%m-%d_%H:%M:%S'
@@ -61,6 +61,7 @@ class Settings(metaclass=Singleton):
                 'sft_script': 'scripts/run_sft_params.sh',
                 'merge_script': 'scripts/run_merge_params.sh',
                 'eval_script': 'scripts/run_eval_params.sh',
+                'ceval_script': 'scripts/run_ceval_params.sh',
                 'logging_steps': '10',
                 'save_steps': '1000',
             },
@@ -68,6 +69,7 @@ class Settings(metaclass=Singleton):
                 'task_id': "task_" + datetime.now().strftime(self.datatime_sft),
                 'train_data': "self_cognition",
                 'eval_data': "self_cognition",
+                'eval_task': "ceval",
                 'checkpoint_path': self.model_dir + "sft_checkpoint/",
                 'output_path': self.model_dir + "merge_output/",
                 'eval_path': self.model_dir + "eval_result/",
@@ -91,6 +93,15 @@ class Settings(metaclass=Singleton):
                 'max_samples': '512',
                 'logging_steps': '10',
                 'save_steps': '1000',
+            },
+            'eval_general': {
+                'eval_task_type': ['bleu&rouge', 'ceval', 'mmlu', 'cmmlu'],
+                'ceval': 'validation',
+                'mmlu': 'validation',
+                'cmmlu': 'test',
+                'lang': "zh",
+                'n_shot': "5",
+                'batch_size': "4",
             },
             'chatglm2': {
                 'template': 'chatglm2',
@@ -116,7 +127,212 @@ class Settings(metaclass=Singleton):
                 'template': 'baichuan2',
                 'finetuning_type': 'lora',
                 'lora_target': 'W_pack',
+            },
+            'qwen': {
+                'template': 'qwen',
+                'finetuning_type': 'lora',
+                'lora_target': 'c_attn',
+            },
+            'mistral': {
+                'template': 'mistral',
+                'finetuning_type': 'lora',
+                'lora_target': 'q_proj,v_proj',
+            },
+            'xverse': {
+                'template': 'xverse',
+                'finetuning_type': 'lora',
+                'lora_target': 'q_proj,v_proj',
+            },
+            'intern': {
+                'template': 'intern',
+                'finetuning_type': 'lora',
+                'lora_target': 'q_proj,v_proj',
             }
         }
+        self.hyper_params_desc = [
+            {
+                "name": "epochs",
+                "title": "训练轮次",
+                "type": "str", #str/number/bool
+                "method": ["all"], #all/full/lora
+                "action": ["all"], #train/merge/eval
+                "stage": ["all"], #pt/sft/rm/ppo/dpo
+                "required": True,
+                "default": "1",
+                "choices_type": "range", #range数值范围/list列表
+                "choices": [
+                    1, 1000
+                ],
+                "help": "控制训练过程中迭代轮数",
+            },
+            {
+                "name": "per_device_train_batch_size",
+                "title": "批处理大小",
+                "type": "str", #str/number/bool
+                "method": ["all"], #all/full/lora
+                "action": ["all"], #train/merge/eval
+                "stage": ["all"], #pt/sft/rm/ppo/dpo
+                "required": True,
+                "default": "1",
+                "choices_type": "range", #range数值范围/list列表
+                "choices": [
+                    1, 32
+                ],
+                "help": "在每次训练迭代中使用的样本数",
+            },
+            {
+                "name": "lr",
+                "title": "学习率",
+                "type": "str", #str/number/bool
+                "method": ["all"], #all/full/lora
+                "action": ["all"], #train/merge/eval
+                "stage": ["all"], #pt/sft/rm/ppo/dpo
+                "required": True,
+                "default": "0.00002",
+                "choices_type": "range", #range数值范围/list列表
+                "choices": [
+                    0, 1
+                ],
+                "help": "AdamW优化器的初始学习率",
+            },
+            {
+                "name": "max_source_length",
+                "title": "输入序列最大长度",
+                "type": "number", #str/number/bool
+                "method": ["all"], #all/full/lora
+                "action": ["all"], #train/merge/eval
+                "stage": ["all"], #pt/sft/rm/ppo/dpo
+                "required": True,
+                "default": "512",
+                "choices_type": "range", #range数值范围/list列表
+                "choices": [
+                    8, 32768
+                ],
+                "help": "输入序列分词后的最大token长度",
+            },
+            {
+                "name": "max_target_length",
+                "title": "输出序列最大长度",
+                "type": "number", #str/number/bool
+                "method": ["all"], #all/full/lora
+                "action": ["all"], #train/merge/eval
+                "stage": ["all"], #pt/sft/rm/ppo/dpo
+                "required": True,
+                "default": "512",
+                "choices_type": "range", #range数值范围/list列表
+                "choices": [
+                    8, 32768
+                ],
+                "help": "输出序列分词后的最大token长度",
+            },
+            {
+                "name": "gradient_accumulation_steps",
+                "title": "梯度累积",
+                "type": "str", #str/number/bool
+                "method": ["all"], #all/full/lora
+                "action": ["all"], #train/merge/eval
+                "stage": ["all"], #pt/sft/rm/ppo/dpo
+                "required": True,
+                "default": "1",
+                "choices_type": "range", #range数值范围/list列表
+                "choices": [
+                    1, 32
+                ],
+                "help": "梯度累积的步数",
+            }
+        ]
+        self.additional_params_desc = [
+            {
+                "name": "lr_scheduler_type",
+                "title": "学习率调度策略",
+                "type": "str", #str/number/list/bool
+                "method": ["all"], #all/full/lora
+                "action": ["all"], #train/merge/eval
+                "stage": ["all"], #pt/sft/rm/ppo/dpo
+                "required": True,
+                "default": "cosine",
+                "choices_type": "list", #range数值范围/list列表
+                "choices": [
+                    "cosine", "linear", "polynomial", "constant", "constant_with_warmup",
+                    "cosine_with_restarts", "inverse_sqrt", "reduce_lr_on_plateau"
+                ],
+                "help": "选择学习率调度策略的算法",
+            },
+            {
+                "name": "quantization_bit",
+                "title": "量化微调",
+                "type": "str", #str/number/bool
+                "method": ["all"], #all/full/lora
+                "action": ["all"], #train/merge/eval
+                "stage": ["all"], #pt/sft/rm/ppo/dpo
+                "required": True,
+                "default": "",
+                "choices_type": "list", #range数值范围/list列表
+                "choices": [
+                    "4", "8"
+                ],
+                "help": "启用量化微调的比特大小",
+            },
+            {
+                "name": "max_samples",
+                "title": "样本数",
+                "type": "number", #str/number/bool
+                "method": ["all"], #all/full/lora
+                "action": ["all"], #train/merge/eval
+                "stage": ["all"], #pt/sft/rm/ppo/dpo
+                "required": True,
+                "default": "512",
+                "choices_type": "range", #range数值范围/list列表
+                "choices": [
+                    0, 2048
+                ],
+                "help": "训练最大样本数",
+            },
+            {
+                "name": "logging_steps",
+                "title": "日志metric输出步数",
+                "type": "number", #str/number/bool
+                "method": ["all"], #all/full/lora
+                "action": ["all"], #train/merge/eval
+                "stage": ["all"], #pt/sft/rm/ppo/dpo
+                "required": True,
+                "default": "5",
+                "choices_type": "range", #range数值范围/list列表
+                "choices": [
+                    5, 1000
+                ],
+                "help": "经过步数后打印metric",
+            },
+            {
+                "name": "save_steps",
+                "title": "检查点保存步数",
+                "type": "number", #str/number/bool
+                "method": ["all"], #all/full/lora
+                "action": ["all"], #train/merge/eval
+                "stage": ["all"], #pt/sft/rm/ppo/dpo
+                "required": True,
+                "default": "10",
+                "choices_type": "range", #range数值范围/list列表
+                "choices": [
+                    10, 10000
+                ],
+                "help": "经过步数后保存checkpoint检查点",
+            },
+            {
+                "name": "precision",
+                "title": "模型精度",
+                "type": "str", #str/number/bool
+                "method": ["all"], #all/full/lora
+                "action": ["all"], #train/merge/eval
+                "stage": ["all"], #pt/sft/rm/ppo/dpo
+                "required": True,
+                "default": "1",
+                "choices_type": "list", #range数值范围/list列表
+                "choices": [
+                    "fp16", "bf16"
+                ],
+                "help": "模型保存的精度粒度",
+            }
+        ]
 
 
